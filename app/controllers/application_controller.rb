7132979fd168c1721d03b4479e9180d8ed46fece
Alex <azuev061981@gmail.com>
2019-08-02 16:39:53 +0300

    next commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_account_update_params, if: :devise_controller?
 
   before_action :require_login
-  before_action :drop_down_list
+  before_action :drop_down_list, :drop_down_list_sort
 
 
 
@@ -21,6 +21,36 @@ class ApplicationController < ActionController::Base
 
   def drop_down_list
     @category_options = Category.all.map{|u| [ u.title, u.id ] }.sort{|a, b| a[0] <=> b[0]}
+  end
+
+  def drop_down_list_sort
+    # com = Comment.order('image_id').group('image.id')
+    com = Image.joins( :comments).group('images.id').size
+    img = Image.joins( :likes).group('images.id').size
+
+    # raise dd
+    # img = Image.joins(:likes, :comments).group('images.id')
+    # @top_categories = Category.joins(img).group('categories.id').order('count(images.id) + count(fans.id) + count(comments.id) DESC')
+    # img = Image.joins(:fans, :comments).group('image.id').order('count(fans.id) + count(comments.id) as total')
+    # @top_categories = Category.joins(img).group('categories.id').order('count(images.id) + total DESC')
+    #
+    # img = Image.joins(:fans, :comments).group('images.id')
+    # @top_categories = Category.joins(img).group('categories.id').order('count(images.id) + count(fans.id) + count(comments.id) DESC')
+    #
+    # 
+    #
+    @category_sort_arr = []
+
+    @category_sort = Category.joins(:images).group('categories.id').order('count(images.id) DESC').limit(5).size
+    # raise dd
+
+    @category_sort.each do |i|
+      @category_sort_arr << Category.find(i[0])
+    end
+    @category_options_sort = @category_sort_arr.map{|u| [ u.title, u.id ] }
+
+    # raise dd
+
   end
 
   private

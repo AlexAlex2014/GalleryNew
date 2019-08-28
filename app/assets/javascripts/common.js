f2b7fcac2b22e1376e7fee8e3abb692cc083f051
Alex <azuev061981@gmail.com>
2019-08-28 08:00:08 +0300

    next commit

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -1,4 +1,34 @@
 $(document).ready(function() {
-  $(".main_head").css("min-height", $(window).height());
-  
+    function heightDetect() {
+        $(".main_head").css("height", $(window).height());
+    };
+
+    heightDetect();
+    $(window).resize(function() {
+      heightDetect();
+    });
+
+    $(".toggle_mnu").on('click', function() {
+        $(".sandwich").toggleClass("active");
+    });
+
+    $(".top_mnu ul a").on('click', function() {
+        $(".top_mnu").fadeOut(600);
+        $(".sandwich").toggleClass("active");
+    });
+
+    $(".toggle_mnu").on('click', function () {
+        if ($(".top_mnu").is(":visible")) {
+            $(".top_mnu").fadeOut(600);
+            $(".top_mnu li a").removeClass("fadeInUp animated")
+        } else {
+            $(".top_mnu").fadeIn(600);
+            $(".top_mnu li a").addClass("fadeInUp animated")
+        };
+    });
 });
+
+// $(window).on('load', function() {
+//   $(".loader_inner").fadeOut();
+//   $(".loader").delay(400).fadeOut("slow");
+// });

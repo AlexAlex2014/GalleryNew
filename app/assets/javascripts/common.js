a30e85148da1733d5d506e36cad99641188d4882
Alex <azuev061981@gmail.com>
2019-08-29 19:07:25 +0300

    next commit

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -1,41 +1,50 @@
-$(document).ready(function() {
-    function heightDetect() {
-        $(".main_head").css("height", $(window).height());
-    };
-
-    heightDetect();
-    $(window).resize(function() {
-      heightDetect();
-    });
-
-    $(".toggle_mnu").on('click', function() {
-        $(".sandwich").toggleClass("active");
-    });
-
-    $(".top_mnu ul a").on('click', function() {
-        $(".top_mnu").fadeOut(600);
-        $(".sandwich").toggleClass("active");
-    });
-
-    $(".toggle_mnu").on('click', function () {
-        if ($(".top_mnu").is(":visible")) {
-            $(".top_text").removeClass("h_opacify");
-            $(".top_mnu").fadeOut(600);
-            $(".top_mnu li a").removeClass("fadeInUp animated");
-        } else {
-            $(".top_text").addClass("h_opacify");
-            $(".top_mnu").fadeIn(600);
-            $(".top_mnu li a").addClass("fadeInUp animated");
+$(document).on('turbolinks:load', function() {
+    $(document).ready(function() {
+        function heightDetect() {
+            $(".main_head").css("height", $(window).height());
         };
-    });
 
-    $('section h2').each(function() {
-        var ths = $(this);
-        ths.html(ths.html().replace(/^(\S+)/, '<span>$1</span>'));
+        heightDetect();
+        $(window).resize(function() {
+            heightDetect();
+        });
+
+        $(".toggle_mnu").on('click', function() {
+            $(".sandwich").toggleClass("active");
+        });
+
+        $(".top_mnu ul a").on('click', function() {
+            $(".top_mnu").fadeOut(600);
+            $(".sandwich").toggleClass("active");
+        });
+
+        $(".toggle_mnu").on('click', function () {
+            if ($(".top_mnu").is(":visible")) {
+                $(".top_text").removeClass("h_opacify");
+                $(".top_mnu").fadeOut(600);
+                $(".top_mnu li a").removeClass("fadeInUp animated");
+            } else {
+                $(".top_text").addClass("h_opacify");
+                $(".top_mnu").fadeIn(600);
+                $(".top_mnu li a").addClass("fadeInUp animated");
+            };
+        });
+
+        $('section h2').each(function() {
+            var ths = $(this);
+            ths.html(ths.html().replace(/^(\S+)/, '<span>$1</span>'));
+        });
     });
 });
 
-// $(window).on('load', function() {
-//   $(".loader_inner").fadeOut();
-//   $(".loader").delay(400).fadeOut("slow");
-// });
+
+$(document).on('turbolinks:load', function() {
+    var script = document.createElement('script');
+    script.src = 'https://cdnjs.cloudflare.com/ajax/libs/fotorama/4.6.4/fotorama.min.js';
+    script.type = 'text/javascript';
+    document.getElementsByTagName('head')[0].appendChild(script);
+
+    $(".loader_inner").fadeOut();
+    $(".loader").delay(400).fadeOut("slow");
+});
+

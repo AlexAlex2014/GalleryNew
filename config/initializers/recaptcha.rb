5fcf0fe8ccc58352c3b9b84de0a57196f1306d13
Alex <azuev061981@gmail.com>
2019-09-02 15:43:24 +0300

    next commit

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,6 +1,6 @@
 Recaptcha.configure do |config|
-  config.site_key  = ENV['RECAPTCHA_PUBLIC_KEY']
-  config.secret_key = ENV['RECAPTCHA_PRIVATE_KEY']
+  config.site_key  = ENV.fetch('RECAPTCHA_PUBLIC_KEY')
+  config.secret_key = ENV.fetch('RECAPTCHA_PRIVATE_KEY')
 
   # config.site_key  = '6LfpWLEUAAAAAHzPhrcypY1ZsZCgiR3TfoRALqzO'
   # config.secret_key = '6LfpWLEUAAAAAO5tEStd9fdYaymGJgKib9Qzr6j1'

bed86673a106bb312c4b19168c61155f42e5b5b1
Alex <azuev061981@gmail.com>
2019-09-02 12:11:29 +0300

    next commit

--- app/assets/javascripts/categories.js
+++ app/assets/javascripts/categories.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $(document).on('click', ".delete-link", function() {
         var current_category_tbody = $(this).parents('.post-box')[0];
-        if(confirm("#{t(:are_you_sure)}")) {
+        if(confirm("Are you sure?")) {
             $.ajax({
                 url: '/categories/' + $(current_category_tbody).attr('data-item_id'),
                 type: 'POST',
@@ -12,28 +12,5 @@ $(document).ready(function(){
             });
         };
     });
-
-
-    // $(document).on('"ajax:success"', ".form_ajax[data-remote]", function() {
-    //
-    //     $.ajax({
-    //         url: '/categories/',
-    //         type: 'POST',
-    //          success: function() {
-    //             $('.form_ajax[data-remote]').parent().html("<%= j render '/shared/category_list', categories: @categories %>");
-    //         }
-    //     });
-    // });
-    // // $(".form_ajax[data-remote]").html("<%= j (render partial: 'categories/category_body') %>")
-    // $('.form_ajax[data-remote]').parent().html("= j render '/shared/category_list', categories: @categories");
 });
 
-
-// $('.form_ajax[data-remote]').html("<%= j render '/shared/category_list', categories: @categories %>");
-
-// $(document).ready ->
-//     $("#new_article").on("ajax:success", (event) ->
-//         [data, status, xhr] = event.detail
-//     $("#new_article").append xhr.responseText
-//     ).on "ajax:error", (event) ->
-//         $("#new_article").append "<p>ERROR</p>"

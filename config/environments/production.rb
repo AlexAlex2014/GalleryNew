9c06c17bee0bed26ad985924c562615cef5dd503
Alex <azuev061981@gmail.com>
2019-09-03 13:27:08 +0300

    next commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       address: 'smtp.gmail.com',
-      port: 587,
+      port: '587',
       domain: 'mail.google.com',
       user_name:    ENV.fetch('USERNAME'),
       password:     ENV.fetch('PASSWORD'),

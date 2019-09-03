da18486744bb91cfbab5379ac4b35f2f51ad2aea
Alex <azuev061981@gmail.com>
2019-09-03 14:58:35 +0300

    next commit

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,6 +1,6 @@
-uri = URI.parse("redis://localhost:6379/")
-Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-Dir["#{Rails.root}/app/jobs/*.rb"].each { |file| require file }
+# uri = URI.parse("redis://localhost:6379/")
+# Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+# Dir["#{Rails.root}/app/jobs/*.rb"].each { |file| require file }
 
 # uri = URI.parse("redis://localhost:6379/")
 # Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :path=>nil, :password=>nil, :db=>0, :driver=>Redis::Connection::Ruby)

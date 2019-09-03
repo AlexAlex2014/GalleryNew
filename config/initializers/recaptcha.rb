025f0daf06d59b3f0ba03cfeb633479a8cd46e2e
Alex <azuev061981@gmail.com>
2019-09-03 10:13:25 +0300

    next commit

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -2,7 +2,4 @@ Recaptcha.configure do |config|
   config.site_key  = ENV.fetch('RECAPTCHA_SITE_KEY')
   config.secret_key = ENV.fetch('RECAPTCHA_SECRET_KEY')
 
-  # config.site_key  = '6LfpWLEUAAAAAHzPhrcypY1ZsZCgiR3TfoRALqzO'
-  # config.secret_key = '6LfpWLEUAAAAAO5tEStd9fdYaymGJgKib9Qzr6j1'
-  # config.proxy = 'http://myproxy.com.au:8080'
 end

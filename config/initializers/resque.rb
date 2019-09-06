49101ffab40db34b8f9cba50446543e2f9798c00
Alex <azuev061981@gmail.com>
2019-09-06 10:22:13 +0300

    next commit

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -3,6 +3,7 @@ if Rails.env.development?
   Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
   Dir["#{Rails.root}/app/jobs/*.rb"].each { |file| require file }
 else
+  ENV['REDISTOGO_URL'] = 'redis://h:p1ebc00fd1211ae2a744a0ec54b792ae7f8764fd4d0aae75bb9cad1d0ef80b992@ec2-3-221-70-198.compute-1.amazonaws.com:15069'
   uri = URI.parse(ENV['REDISTOGO_URL'])
   REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
   Resque.redis = REDIS

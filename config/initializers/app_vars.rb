8efc5fe27df6673f312c6cad175faa64b8e68c1a
Alex <azuev061981@gmail.com>
2019-09-19 12:15:12 +0300

    next commit

---
+++ config/initializers/app_vars.rb
@@ -0,0 +1,20 @@
+ENV['USERNAME'] = 'azuev3199@gmail.com'
+ENV['PASSWORD'] = '3199azuev'
+
+
+ENV['RECAPTCHA_SITE_KEY'] = '6LfpWLEUAAAAAHzPhrcypY1ZsZCgiR3TfoRALqzO'
+ENV['RECAPTCHA_SECRET_KEY'] = '6LfpWLEUAAAAAO5tEStd9fdYaymGJgKib9Qzr6j1'
+
+ENV['DB_NAME'] = 'appgallery_development'
+ENV['DB_NAME_PRO'] = 'gallerynew'
+ENV['DB_USERNAME'] = 'developer'
+ENV['DB_PASS'] = '123456'
+
+ENV['facebook_app_id'] = '410172693177349'
+ENV['facebook_app_secret'] = '050568d76a9e9afece84dda54dde802c'
+
+
+ENV['ACCESS_KEY_ID'] = 'AKIA54YFOF6L6VDIFAMN'
+ENV['SECRET_ACCESS_KEY'] = '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'
+ENV['REGION'] = 'eu-west-3'
+ENV['NAME_OF_BUCKET'] = 'gentle-ridge-18466'

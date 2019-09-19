9ae3bd82ad994ab8135e53e3b5c93d517dfa5136
Alex <azuev061981@gmail.com>
2019-09-19 12:56:03 +0300

    next commit

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,11 +1,11 @@
-if Rails.env.development?
-  uri = URI.parse("redis://localhost:6379/")
-  Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-else
-  # ENV['REDISTOGO_URL'] = 'redis://h:p1ebc00fd1211ae2a744a0ec54b792ae7f8764fd4d0aae75bb9cad1d0ef80b992@ec2-3-221-70-198.compute-1.amazonaws.com:15069'
-  uri = URI.parse(ENV['REDISTOGO_URL'])
-  REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-  Resque.redis = REDIS
-  # Dir["#{Rails.root}/app/jobs/*.rb"].each { |file| require file }
-end
-Dir["#{Rails.root}/app/jobs/*.rb"].each { |file| require file }
+# if Rails.env.development?
+#   uri = URI.parse("redis://localhost:6379/")
+#   Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+# else
+#   # ENV['REDISTOGO_URL'] = 'redis://h:p1ebc00fd1211ae2a744a0ec54b792ae7f8764fd4d0aae75bb9cad1d0ef80b992@ec2-3-221-70-198.compute-1.amazonaws.com:15069'
+#   uri = URI.parse(ENV['REDISTOGO_URL'])
+#   REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+#   Resque.redis = REDIS
+#   # Dir["#{Rails.root}/app/jobs/*.rb"].each { |file| require file }
+# end
+# Dir["#{Rails.root}/app/jobs/*.rb"].each { |file| require file }

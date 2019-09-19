064373ba942d79ef9caca9f948e570777f9390e7
Alex <azuev061981@gmail.com>
2019-09-19 13:02:33 +0300

    next commit

--- config/environment.rb
+++ config/environment.rb
@@ -2,7 +2,8 @@
 require_relative 'application'
 
 # Initialize the Rails application.
-Rails.application.initialize!
 
 app_vars = File.join(Rails.root, 'config', 'initializers', 'app_vars.rb')
 load(app_vars) if File.exists?(app_vars)
+
+Rails.application.initialize!

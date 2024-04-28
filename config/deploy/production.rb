56684eec0f850c51785ccbe709923a35882974fb
alexz <azuev061981@gmail.com>
2024-04-28 22:44:06 +0300

    Set up Puma, Nginx & Capistrano

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -17,9 +17,9 @@
 # property set. Specify the username and a domain or IP for the server.
 # Don't use `:all`, it's a meta role.
 
-role :app, %w{deploy_user@3.122.251.214}
-role :web, %w{deploy_user@3.122.251.214}
-role :db,  %w{deploy_user@3.122.251.214}
+role :app, %w{alex@146.120.162.145}
+role :web, %w{alex@146.120.162.145}
+role :db,  %w{alex@146.120.162.145}
 
 
 # Configuration
@@ -40,11 +40,11 @@ role :db,  %w{deploy_user@3.122.251.214}
 #
 # Global options
 # --------------
- set :ssh_options, {
-   keys: %w(~/Desktop/gallery_keypair.pem),
-   forward_agent: false,
-   auth_methods: %w(publickey password)
- }
+#  set :ssh_options, {
+#    keys: %w(~/Desktop/gallery_keypair.pem),
+#    forward_agent: false,
+#    auth_methods: %w(publickey password)
+#  }
 #
 # The server-based syntax can be used to override options:
 # ------------------------------------

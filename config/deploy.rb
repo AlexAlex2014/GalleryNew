5c55d9db4f770715c0348c0ab912112efa873a5c
alexz <azuev061981@gmail.com>
2024-06-16 16:30:18 +0300

    fix deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,7 @@ set :stage,           :production
 set :deploy_via,      :remote_cache
 # set :deploy_to,     "/var/www/gallery_new"
 set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
-set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+# set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
@@ -48,7 +48,7 @@ set :puma_init_active_record, true  # Change to false when not using ActiveRecor
 
 # Default value for :linked_files is []
 # append :linked_files, "config/database.yml"
-set :linked_files, %w[ config/database.yml ]
+# set :linked_files, %w[ config/database.yml ]
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 # Rake::Task["deploy:assets:backup_manifest"].clear_actions

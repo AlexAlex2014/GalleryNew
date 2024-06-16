67b796c35f4a9bbaa5f60a2ff8daa1e8a8ef215f
alexz <azuev061981@gmail.com>
2024-06-16 17:23:19 +0300

    fix deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,14 +27,21 @@ set :deploy_via,      :remote_cache
 # set :deploy_to,     "/var/www/gallery_new"
 set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
 # set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
-set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
-set :puma_access_log, "#{release_path}/log/puma.error.log"
-set :puma_error_log,  "#{release_path}/log/puma.access.log"
+# set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+# set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+# set :puma_access_log, "#{release_path}/log/puma.error.log"
+# set :puma_error_log,  "#{release_path}/log/puma.access.log"
 set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_ed25519.pub) }
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
+set :puma_threads, [4, 16]
+set :puma_workers, 0
+
+set :pty, true
+set :use_sudo, false
+
+set :rails_env, 'production'
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
